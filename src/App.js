6387e9eca51369c101990bf82c5cf4913f35f4a2
Ifeoluwadayo Noah <ogunderuife@yahoo.com>
2022-01-24 18:35:41 +0100

    redo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React, { useState } from 'react'
-import axios from 'axios';
 import image1 from './Images/searchh.svg'
+import axios from 'axios'
 
 function Navbar() {
 
     const [location, setLocation] = useState('');
     const [data, setData] = useState({});
+    const [days, setDays] = useState({})
     
     const date = new Date().toLocaleDateString();
     
-
-
     // `api.openweathermap.org/data/2.5/forecast/daily?q=${location}&cnt=5&appid=9aa887d6139b52879968085ce16d71b6`
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=9aa887d6139b52879968085ce16d71b6`
+    const url_one = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=9aa887d6139b52879968085ce16d71b6`
+    const url_two = `https://api.openweathermap.org/data/2.5/forecast/?q=${location}&cnt=5&units=metric&appid=9aa887d6139b52879968085ce16d71b6`
 
   
     const handleSearch = (event ) => {
@@ -23,16 +23,18 @@ function Navbar() {
 
         event.preventDefault();
 
-        axios.get(url).then((response) => {
+        axios.get(url_one).then((response) => {
           setData(response.data)
           console.log(response.data)
         })
 
+        axios.get(url_two).then((response) => {
+          setDays(response.data)
+          console.log(response.data)
+        })
         setLocation('')
       }
     }
-    
-
 
     return (
   
@@ -46,6 +48,7 @@ function Navbar() {
           <div className = "date">
             { date }
           </div>
+
         </div>
   
         <div className = "banner-content">
@@ -63,21 +66,89 @@ function Navbar() {
             />
           </div>
 
-          <div className = "location">
-            { data.name ? <h2>{data.name}, {data.sys.country}</h2> : null}
-          </div>
+          {
+            data.main && (
 
-          <div className = "temp"> 
-            { data.main ? <h2>{data.main.temp.toFixed()}°c</h2> : null}
-          </div>
+              <div className = "wrapper">
 
-          <div className = "description"> 
-          { data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+                <div className = "info">
+                  <h2 className = "location"> {data.name}, {data.sys.country} </h2>
 
-          <div className = "hi-lo">
-          { data.main ? <h2>{Math.round(data.main.temp_min)}°c/{Math.round(data.main.temp_max)}°c </h2> : null}
-          </div>
+                  <div className = "data-box">
+                    <h2 className = "temp"> {data.main.temp.toFixed()}<sup>°c</sup></h2>
+                    <h6 className = "hi-lo"> {Math.round(data.main.temp_min)}<sup>°c</sup>/{Math.round(data.main.temp_max)}<sup>°c</sup> </h6>
+                  </div>
+                </div>
+
+                <div className = "description">
+                  <img className = "icon" src = {`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+                  <h2 className = ""> {data.weather[0].main} </h2>
+                </div>
+
+              </div>
+            )
+          }
+
+          { days.list && (
+
+            <div className = "days-container">
+
+              <div className = "days">
+                <h1 className = "date"> {days.list[0].dt_txt} </h1>
+
+                <div className = "temp-holder">
+                  <h2 className = "day-temp">{days.list[0].main.temp.toFixed()}<sup>°c</sup></h2>
+                  <h6 className = "day-hi-lo"> {Math.round(days.list[0].main.temp_min)}<sup>°c</sup>/{Math.round(days.list[0].main.temp_max)}<sup>°c</sup> </h6>
+                </div>
+                {/* <h2> {days.list[0].weather[0].main} </h2> */}
+                <img src = {`https://openweathermap.org/img/wn/${days.list[0].weather[0].icon}@2x.png`} />
+              </div>
+
+              <div className = "days">
+                <h1 className = "date"> {days.list[1].dt_txt} </h1>
+                <div className = "temp-holder">
+                  <h2 className = "day-temp">{days.list[1].main.temp.toFixed()}<sup>°c</sup></h2>
+                  <h6 className = "day-hi-lo"> {Math.round(days.list[1].main.temp_min)}<sup>°c</sup>/{Math.round(days.list[1].main.temp_max)}<sup>°c</sup> </h6>
+                </div>
+                {/* <h2> {days.list[0].weather[0].main} </h2> */}
+                <img src = {`https://openweathermap.org/img/wn/${days.list[1].weather[0].icon}@2x.png`} />
+              </div>
+
+              <div className = "days">
+                <h1 className = "date"> {days.list[2].dt_txt} </h1>
+                <div className = "temp-holder">
+                  <h2 className = "day-temp">{days.list[2].main.temp.toFixed()}<sup>°c</sup></h2>
+                  <h6 className = "day-hi-lo"> {Math.round(days.list[2].main.temp_min)}<sup>°c</sup>/{Math.round(days.list[2].main.temp_max)}<sup>°c</sup> </h6>
+                </div>
+                {/* <h2> {days.list[0].weather[0].main} </h2> */}
+                <img src = {`https://openweathermap.org/img/wn/${days.list[2].weather[0].icon}@2x.png`} />
+              </div>
+
+              <div className = "days">
+                <h1 className = "date"> {days.list[3].dt_txt} </h1>
+                <div className = "temp-holder">
+                  <h2 className = "day-temp">{days.list[3].main.temp.toFixed()}<sup>°c</sup></h2>
+                  <h6 className = "day-hi-lo"> {Math.round(days.list[3].main.temp_min)}<sup>°c</sup>/{Math.round(days.list[3].main.temp_max)}<sup>°c</sup> </h6>
+                </div>
+                {/* <h2> {days.list[0].weather[0].main} </h2> */}
+                <img src = {`https://openweathermap.org/img/wn/${days.list[3].weather[0].icon}@2x.png`} />
+              </div>
+
+              <div className = "days">
+                <h1 className = "date"> {days.list[4].dt_txt} </h1>
+                <div className = "temp-holder">
+                  <h2 className = "day-temp">{days.list[4].main.temp.toFixed()}<sup>°c</sup></h2>
+                  <h6 className = "day-hi-lo"> {Math.round(days.list[4].main.temp_min)}<sup>°c</sup>/{Math.round(days.list[4].main.temp_max)}<sup>°c</sup> </h6>
+                </div>
+                {/* <h2> {days.list[0].weather[0].main} </h2> */}
+                <img src = {`https://openweathermap.org/img/wn/${days.list[4].weather[0].icon}@2x.png`} />
+              </div>
+
+              
+            </div>
+
+          )}
+
           
         </div>
           
